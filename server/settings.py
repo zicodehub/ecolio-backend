c058e33b7e5a26dc14b2828c49c006ebec3610d6
zicode <marcleord@gmail.com>
2021-09-09 23:34:36 +0000

    Heroku configs

--- server/settings.py
+++ server/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+import django_heroku
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-zh^9@om#3udjv@6k33r4d*n@_h+8a^j4)0ow^!^#wwp)n4!!_a'
+# SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = "django-insecure-zh^9@om#3udjv@6k33r4d*n@_h+8a^j4)0ow^!^#wwp)n4!!_a"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -80,6 +82,7 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -281,3 +284,6 @@ DATABASE_ROUTERS = [
     'core.routers.TIRRouter',    
 ]
 
+
+
+django_heroku.settings(locals())

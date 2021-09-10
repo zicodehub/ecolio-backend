b8f7abc9893ba57105d5353fd9c2d27342db0d5f
zicode <marcleord@gmail.com>
2021-09-10 00:38:31 +0000

    Allow any host

--- server/settings.py
+++ server/settings.py
@@ -94,12 +94,12 @@ MIDDLEWARE = [
 
 ]
 
-# CORS_ORIGIN_ALLOW_ALL = True
-ALLOWED_HOSTS=['localhost', '127.0.0.1']
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
-]
+CORS_ORIGIN_ALLOW_ALL = True
+ALLOWED_HOSTS=[ '*' ]
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:3000",
+#     "http://127.0.0.1:3000",
+# ]
 
 
 

c45eda85bd1c9ab4099334314dd6600091ca56b3
zicode <marcleord@gmail.com>
2021-09-13 15:04:08 +0000

    Filtering enabled on Users

--- app_api/views.py
+++ app_api/views.py
@@ -39,8 +39,8 @@ class UserViewSet(CustomListing, generics.ListCreateAPIView):
     """
     queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
-    # filterset_fields = ('id', )
-    # filter_backends = [ DjangoFilterBackend ]
+    filterset_fields = ('id', )
+    filter_backends = [ DjangoFilterBackend ]
     # filterset_fields = '__all__'
 
     def create(self, request, **kw) :
